7ac5adaee355a85b8950bc168cacd2d68351b68c
noorulain88 <noorulainain88@gmail.com>
2025-11-14 20:09:19 +0500

    Feat: practice 4 5 6

---
+++ Pratice5Oprs/program.js
@@ -0,0 +1,185 @@
+console.log("Hello world!");
+
+// program to check user name
+// var username=prompt("enter your name");
+// if(username=="noor"){
+//     console.log("welome noor !")
+// }
+// else{
+//     console.log("welcome guset !")
+// }
+
+// program to check trip premission
+// var trip=prompt("You want to go trip ?yes/no");
+// if(trip=="yes"){
+//     console.log("enjoy your trip")
+// }
+// else{
+//     console.log("stay home and enjoy sleeping")
+// }
+
+// program to check weather
+// var weather=prompt("is it raining outside ? yes or no")
+// if(weather=="yes"){
+//     console.log("enojy rainy day")
+// }
+// else{
+//     console.log("wait for rainy day")
+// }
+// program to check their study time
+// var study= prompt(parseInt ("enter your study time "))
+// if(study=="6"){
+//     console.log("good job , keep it up")
+// }
+// else{(study<6)
+//     console.log(" you need to study more" )
+// }
+
+// program to check your daily routine
+// var routine=prompt("do you have a daily routine ? yes/no")
+// if(routine=="yes"){
+//     console.log("good to hear that")
+// }
+// else{
+//     console.log("try to make a daily routine")
+// }
+
+
+// arrays
+
+// var city0="lahore"
+// var city1="karachi"
+// var city2="pishawar"
+// var city3="queeta"
+// var city4="shaikopora"
+// var city5="sahiwal"
+// var city6="multan"
+// var city7="naran"
+// // var cities=[Element1,Element2, element3,Element4,element5,Element7,]
+
+// // var arrays_name=[element1,element2,]
+// console.log("THE last city",city7 )
+// console.log("THE fifth city",city5 )
+// console.log("THE fourth city",city4 )
+// console.log("THE sixth city",city6 )
+// the code without arrays
+
+// for (var cities= 0; cities<9;cities++){
+//     console.log("slskgjg",cities)
+// }
+// wrong
+
+
+
+// var num={ 12 , 45 , 67 }
+// console.log("numbreer",num)
+// console.log("number0",[0])
+// console.log("number0",[2])
+// console.log("number0",[3])
+
+
+
+// var country=["eurlope",
+//     "england", "farnace",]
+// for(var i = 0; i<country.length;i++){
+//     console.log(country[i])
+// }
+
+// var student=prompt("enter your name ")
+// for (var num=0;num<5;num++){
+//     console.log("user age is ", num )
+// }
+
+
+
+// var cities=["lahore",
+//     "rawalpindi",
+//     "shakpoora",
+//     "sailkot",
+//       "multan",
+
+// ]
+
+
+//     console.log("the cities name " ,cities [3])
+//     console.log("the cities name " ,cities[4])
+//     console.log("the cities name " ,cities[0])
+//     console.log("the cities name " ,cities[2])
+
+
+
+// hard work of aray
+
+// var studentName = [];
+// // studentName[0]=prompt("Enter first student name")
+// // studentName[1]=prompt("Enter second student name");
+// // studentName[2]=prompt("Enter second student name");
+// // studentName[3]=prompt("Enter second student name");
+// // studentName[4]=prompt("Enter second student name");
+
+// // console.log("studentName",studentName)
+
+// for (var i=0;i<5;i++){
+//     studentName[i]=console.log =prompt("enter student name", + (i+1))
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+    let row = "";
+    for (let j = 1; j <= 10; j++) {
+        row += (i * j) + "\t";
+    }
+    console.log(row);
+}
+
+
+// // nested loop
+// Asked by sir
+// for(var i=0; i<=5; i++){
+//     let row ="";
+//     for(var j=0;j<i;j++){
+//         row+="+"  
+//     }
+//      console.log(row)
+
+// }
+var cities=[
+"lahore",
+"faisalabad",
+"sahiwal",
+"multan",
+"djk",
+
+]
+for(var i=0; i<5;i++){
+    console.log("the", +(1+cities))
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Pratice6JSS/program.js
@@ -0,0 +1,78 @@
+// console.log("Hello, World!");
+// var name ="noor"
+// console.log("my name is",name )
+// console.log("variables in string")
+ 
+// jmflksdjfklds jklfs djfjdsk fklsdjfk
+// jfklsdjfsdkfjsdkfj
+
+
+
+
+// fdsmfdsfsdffsdfsdfsdf
+// hello
+// var age =44
+// console.log("my age is" ,age )
+// var number , string ,and boolean
+// var game ="cricket"
+// console.log("my fav game", game )
+// console.log("variables in boolean")
+
+// var hafiz = "true"
+// console.log("hafiz is true or false" , hafiz
+// )
+// console.log("variables in number ")
+// var height = "5.4"
+// console.log("my height is " ,height )
+// var weight = "70kg"
+// console.log("my weight is " ,weight )
+
+/*comment in a sigle line*/
+// var height = "5.4"
+/*coment in multiple line
+console.log("my height is " ,height )
+var weight = "70kg"
+// console.log("my weight is " ,weight )*/
+// console.log("hello world")
+// var username= "noor"
+// console.log("my name is " , username)
+
+// var age=45
+// console.log("my age is " , age)
+// var userClass = 7
+// console.log("my class is", userClass)
+
+// var fatherName= "munir ul hassan"
+// console.log("my father name is" , fatherName)
+
+// var schoolName="laboratory school"
+// console.log("my schoool name is" , schoolName)
+// var height= 5.6
+// console.log("my height is " , height)
+// var weigth= 40
+// console.log("my weigth is " , weigth)
+// var course="web development"
+// console.log("my course is ", course)
+// var rollNum= 2417
+// console.log("my roll number is" , rollNum)
+// var courseTime= "3 months"
+// console.log("the course time is " ,courseTime)
+// var language="html css and javascript"
+// console.log ("course include languag is " , language)
+
+
+
+
+// loop and switch
+
+var i=4
+while(i<=10){
+    console.log("the value of i is les the 10" ,i)
+    i++
+
+}
+
+do{
+    console.log("the value is less the i" ,i)
+    i++
+} while(i<=10)
