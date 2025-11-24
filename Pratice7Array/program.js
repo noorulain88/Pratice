04cc0cd63541d333e36072f2886a80986430ce7d
noorulain88 <noorulainain88@gmail.com>
2025-11-24 20:29:33 +0500

    pratice

---
+++ Pratice7Array/program.js
@@ -0,0 +1,66 @@
+// console.log("HEllO WORLD!")
+
+// // push
+// var student=['noor','aleena','tania'];
+// student.push("tania");
+// console.log("the student name",student)
+
+// // pop
+// var age=['56','67','78','90'];
+// age.pop();
+// console.log("the value of age is ",age)
+
+// // the number 
+
+// // 
+
+//  finding segment  in a string
+
+
+
+// var sentence='It is startling to think that, even in the darkest depths of World War II, J. R. R.olkien writing the trilogy, which contains, with the weird applicability availableonl ytopoetrmyth, the essential notion that the good gray wizard can understand the evil magi precisel because he is just enough like them to grasp their minds and motives in ways that they can'
+// for (var i=0;i<sentence.length;i++){
+// if( sentence.slice (i,i+12)=='World War II'){
+//     sentence=sentence.slice(0,i) + 'seconde war  II '+ sentence.slice(i+12);
+//     console.log(sentence)
+// }
+// }
+// var sentence='It is startling to think that, even in the darkest depths of World War II, J. R. R.olkien writing the trilogy, which contains, with the weird applicability availableonl ytopoetrmyth, the essential notion that the good gray wizard can understand the evil magi precisel because he is just enough like them to grasp their minds and motives in ways that they can'
+
+// for(var i=0;i<sentence.length;i++){
+//     if(sentence.slice(i,i+12)=='World War II'){
+//         sentence=sentence.slice(0,i) + 'seconde war II' + sentence.slice(i +12);
+//         console.log("sentence" ,sentence)
+//     }
+// }
+
+// var sentence='It is startling to think that, even in the darkest depths of World War II, J. R. R.olkien writing the trilogy, which contains, with the weird applicability availableonl ytopoetrmyth, the essential notion that the good gray wizard can understand the evil magi precisel because he is just enough like them to grasp their minds and motives in ways that they can'
+ 
+// for(var i=0;i<sentence.length;i++){
+//     if(sentence.slice(i,i+12)=='World War II'){
+//         sentence=sentence.slice(0,i) + 'secone war II' + sentence.slice(i +12);
+//         console.log("sentence" ,sentence)
+// }
+// }
+
+
+// another method indexof
+// var sentence='It is startling to think that, even in the darkest depths of World War II, J. R. R.olkien writing the trilogy, which contains, with the weird applicability availableonl ytopoetrmyth, the essential notion that the good gray wizard can understand the evil magi precisel because he is just enough like them to grasp their minds and motives in ways that they can'
+
+// measuring length of string in array
+// var Names=['noor','sleena','tania','maria','fatima'];
+// console.log("the length of array is ",Names.slice(0,Names.length));
+
+
+// measuring length of string elements
+// 
+
+
+// alert
+
+
+
+
+
+
+
