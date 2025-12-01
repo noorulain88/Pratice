4328973285259abca81f4fb1b2bc7fad5d49c84b
noorulain88 <noorulainain88@gmail.com>
2025-12-01 20:24:46 +0500

    pratice

---
+++ Function/progrma.js
@@ -0,0 +1,143 @@
+// user age and name show by function
+// var UserAge=parseInt(prompt("Enter your age"))
+
+
+// function age(UserAge ){
+//   if(UserAge<18){
+//     console.log("you are not greater then 18")
+
+//   }else{
+//     console.log("users age is greater then 18")
+//   }
+//   }
+
+// age(UserAge)
+// var userName=prompt("enter your name")
+// function Name(userName){
+//     console.log("user name is " ,userName)
+// }
+// Name(userName)
+  
+// var userFruit=prompt("enter your fruit name")
+// function Fruit(userFruit){
+//     if (userFruit=="apple"){
+//         console.log("user fruit is apple")
+//     }
+//     else{
+//         console.log("user fruit is not apple")
+//     }
+// }
+// Fruit(userFruit)
+
+// var Weekend=prompt("enter your weekend")
+// function Week(weekend){
+//     if(weekend=="monday"){
+//         console.log("today day is monday" )
+//     }
+//     else{
+//           console.log("today day  not is monday" )
+//     }
+// }
+// Week(Weekend)
+
+// var firstPlayer=prompt("Enter your first player name")
+// var secondePlayer=prompt("Enter your seconde player name")
+// function players(firstPlayer,secondePlayer){
+//     if(firstPlayer=="noor"){
+//         console.log("first player is noor")
+//         if (secondePlayer=="fiza") {
+//             console.log("seconde name is fiza")
+//         } else { 
+//             console.log("user name is not fiza")
+            
+//         }
+//     }
+//     else{
+//         console.log("player is not a noor or fiza")
+//     }
+// }
+// players(firstPlayer,secondePlayer)
+
+// var Day=new Date();
+// console.log("today day is" ,Day)
+
+
+//  var todayDate=new Date();
+// var month=todayDate.getMonth
+// console.log("month " ,month)
+// var Months=['january'
+//     ,'feb'
+//     ,'march',
+//     'apirl'
+//     ,'may','june'
+//     ,'july'
+    // ,'agust'***** wrong
+//     ,'sep',
+//     'nov',
+//     'dec']
+// console.log("The months name",month[Months])
+
+
+// var Day = new Date();
+// var days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+// var date=Day.getDay();
+// console.log("THE current " ,days[date]);
+
+// var Day = new Date();
+// var currentMonth=Day.getMonth();
+// var allMonth=['january','Febraury','March','april','May','June','July','August','September','october','November','December']
+// console.log("currnet month is" , allMonth[ currentMonth]);
+
+// var Day = new Date();
+
+// var Currentyear=Day.getFullYear();
+// console.log("current yeaar is",Currentyear);
+
+// var BDY="nov-8-2008"
+// var Day= new Date();
+// var userBirthDay=new Date(BDY)
+// console.log("user birhtday",userBirthDay)
+
+// var userMilliSeconde=userBirthDay.getTime();
+// console.log("user mili seconde is",userMilliSeconde);
+// var allMilliSeconde=Day.getTime ()
+// console.log("The total milli from 1970 to 2025 is",allMilliSeconde)
+// var difference=allMilliSeconde-userMilliSeconde
+// console.log("differene is ",difference)
+// var age=difference/1000/60/60/24/365
+// console.log("user age is ",Math.floor(age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var userDate = "Oct 17,2000";
+// // console.log("🚀 ~ userDate:", userDate);
+// var dateOfBirth = new Date(userDate);
+// var currentDate = new Date();
+// var millisecondsCurrent = currentDate.getTime(); // 1970 - 2025
+// var miliSecondsDob = dateOfBirth.getTime(); // 1970 - 1997
+
+
+
+
